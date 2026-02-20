19708b23e3c686543607103cd36b8131434e8c8a
LegionaryOfLogic <calebhof11@gmail.com>
2026-02-19 16:25:41 -0800

    added i2c_tools module w/I2C_Bus and i2C_Slave classes. this will replace i2c_bus
    made refactoring changes in clib

--- src/slvrov_tools/clibs/i2c_tools.h
+++ src/slvrov_tools/clibs/i2c_tools.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-int i2c_bus(const char *bus_path);
+int i2c_open_bus(const char *bus_path);
 int i2c_read_byte(int bus, uint8_t slave_addr, uint8_t register_addr);
 int i2c_write_byte(int bus, uint8_t slave_addr, uint8_t register_addr, uint8_t value);
 
